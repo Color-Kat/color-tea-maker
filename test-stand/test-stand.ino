9ffd62498928b0324c7a947ef9cd1d14d1abf48e
Color-Kat <katik7979@gmail.com>
2022-08-09 13:24:08 +0300

    color tea maker works;)

--- test-stand/test-stand.ino
+++ test-stand/test-stand.ino
@@ -1,12 +1,12 @@
-#define kettle_relay_pin A3   // Relay of kettle
+#define kettle_relay_pin A5   // Relay of kettle
 #define hot_pump_pin A4       // Hot water 
-#define mixer_relay_pin A1    // Hot water pump
 #define motor_pin_1 2         // Sugar dispenser 
 #define motor_pin_2 3         // Sugar dispenser 
+#define mixer_relay_pin A1    // Hot water pump
 
 #define switch_button_pin 12
 
-int state = 0;
+int stage = 0;
 unsigned long last_click = 0;
 
 void setup() {
@@ -31,29 +31,44 @@ void loop() {
     if(button == 1 && millis() - last_click > 300){
         last_click = millis();
         
-        state++;
-        if(state > 4) state = 0;
+        stage++;
+        if(stage > 4) stage = 0;
         
-        Serial.println(state);
+        Serial.println(stage);
     }
 
-    if(state == 0) {
+    if(stage == 0) {
         Serial.println("Чайник включён");
+        digitalWrite(kettle_relay_pin, 1);
     }
 
-    if(state == 1) {
+    if(stage == 1) {
         Serial.println("Помпа включена");
+        digitalWrite(kettle_relay_pin, 0);
+        
+        digitalWrite(hot_pump_pin, 1);
     }
 
-    if(state == 2) {
+    if(stage == 2) {
         Serial.println("Дозатор сахара включён");
+        digitalWrite(hot_pump_pin, 0);
+        
+        digitalWrite(motor_pin_1, 0);
+        digitalWrite(motor_pin_2, 1);
     }
 
-    if(state == 3) {
+    if(stage == 3) {
         Serial.println("Включаем мешалку");
+
+//        digitalWrite(motor_pin_1, 1);
+//        digitalWrite(motor_pin_2, 1);
+        
+        digitalWrite(mixer_relay_pin, 1);
     }
 
-    if(state == 4) {
+    if(stage == 4) {
+        digitalWrite(mixer_relay_pin, 0);
+      
         Serial.println("Чай готов!");
     }
 }
