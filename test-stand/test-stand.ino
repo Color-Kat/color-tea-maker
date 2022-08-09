2c96cab31d999cdcf773c71f79816aefc15ad98e
Color-Kat <katik7979@gmail.com>
2022-08-09 16:30:18 +0300

    better code

--- test-stand/test-stand.ino
+++ test-stand/test-stand.ino
@@ -1,74 +1,107 @@
-#define kettle_relay_pin A5   // Relay of kettle
-#define hot_pump_pin A4       // Hot water 
-#define motor_pin_1 2         // Sugar dispenser 
-#define motor_pin_2 3         // Sugar dispenser 
-#define mixer_relay_pin A1    // Hot water pump
+#define kettle_relay_pin A5 // Relay of kettle
+#define hot_pump_pin A4     // Hot water
+#define motor_pin_1 2       // Sugar dispenser
+#define motor_pin_2 3       // Sugar dispenser
+#define mixer_relay_pin A1  // Mixer pin
 
 #define switch_button_pin 12
 
-int stage = 0;
-unsigned long last_click = 0;
+class button {
+  public:
+    button(byte pin) {
+      _pin = pin;
+      pinMode(_pin, INPUT_PULLUP);
+    }
+    bool click() {
+      bool btnState = digitalRead(_pin);
+      if (!btnState && !_flag && millis() - _tmr >= 300) {
+        _flag = true;
+        _tmr = millis();
+        return true;
+      }
+      if (!btnState && _flag && millis() - _tmr >= 300) {
+        _tmr = millis();
+        return true;
+      }
+      if (btnState && _flag) {
+        _flag = false;
+        _tmr = millis();
+      }
+      return false;
+    }
 
-void setup() {
-    Serial.begin(9600);
+  private:
+    byte _pin;
+    uint32_t _tmr;
+    bool _flag;
+};
 
-    pinMode(kettle_relay_pin, OUTPUT);
-    pinMode(hot_pump_pin, OUTPUT);
-    pinMode(mixer_relay_pin, OUTPUT);
-    pinMode(motor_pin_1, OUTPUT);
-    pinMode(motor_pin_2, OUTPUT);
+button sw_button(switch_button_pin); // Class button, object sw_button
 
-    pinMode(switch_button_pin, INPUT_PULLUP);
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(kettle_relay_pin, OUTPUT);
+  digitalWrite(kettle_relay_pin, LOW);
+  pinMode(hot_pump_pin, OUTPUT);
+  digitalWrite(hot_pump_pin, LOW);
+  pinMode(mixer_relay_pin, OUTPUT);
+  digitalWrite(mixer_relay_pin, LOW);
+  pinMode(motor_pin_1, OUTPUT);
+  pinMode(motor_pin_2, OUTPUT);
+  digitalWrite(motor_pin_1, LOW);
+  digitalWrite(motor_pin_2, LOW);
 }
+
+void loop()
+{
+  static uint8_t stage = 0;
   
-void loop() {
-//    digitalWrite(motor_pin_1, 0);
-//    digitalWrite(motor_pin_2, 1);
+  // Switch state of making tea
+  if (sw_button.click()) {
+    stage++;
+    if (stage > 4) stage = 0;
 
-    boolean button = !digitalRead(switch_button_pin);
-
-    // Switch state of making tea
-    if(button == 1 && millis() - last_click > 300){
-        last_click = millis();
-        
-        stage++;
-        if(stage > 4) stage = 0;
-        
-        Serial.println(stage);
-    }
-
-    if(stage == 0) {
+    Serial.println(stage);
+    switch (stage) {
+      case 0:
         Serial.println("Чайник включён");
-        digitalWrite(kettle_relay_pin, 1);
-    }
+        digitalWrite(kettle_relay_pin, HIGH);
+        break;
 
-    if(stage == 1) {
+      case 1:
         Serial.println("Помпа включена");
-        digitalWrite(kettle_relay_pin, 0);
-        
-        digitalWrite(hot_pump_pin, 1);
-    }
+        digitalWrite(kettle_relay_pin, LOW);
 
-    if(stage == 2) {
+        digitalWrite(hot_pump_pin, HIGH);
+        break;
+
+      case 2:
         Serial.println("Дозатор сахара включён");
-        digitalWrite(hot_pump_pin, 0);
-        
-        digitalWrite(motor_pin_1, 0);
-        digitalWrite(motor_pin_2, 1);
-    }
+        digitalWrite(hot_pump_pin, LOW);
 
-    if(stage == 3) {
+        digitalWrite(motor_pin_1, LOW);
+        digitalWrite(motor_pin_2, HIGH);
+        break;
+
+      case 3:
         Serial.println("Включаем мешалку");
 
-//        digitalWrite(motor_pin_1, 1);
-//        digitalWrite(motor_pin_2, 1);
-        
-        digitalWrite(mixer_relay_pin, 1);
-    }
+        digitalWrite(motor_pin_1, LOW);
+        digitalWrite(motor_pin_2, LOW);
 
-    if(stage == 4) {
-        digitalWrite(mixer_relay_pin, 0);
-      
+        digitalWrite(mixer_relay_pin, HIGH);
+        break;
+
+      case 4:
+        digitalWrite(mixer_relay_pin, LOW);
+
         Serial.println("Чай готов!");
+        break;
+
+      default:
+        break;
     }
+  }
 }
