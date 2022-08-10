f7b16beb8215b566e6b0705b3bf2ea1cc2b0653e
Color-Kat <katik7979@gmail.com>
2022-08-10 14:10:09 +0300

    time for stages

--- test-stand/test-stand.ino
+++ test-stand/test-stand.ino
@@ -1,10 +1,15 @@
-#define kettle_relay_pin A5 // Relay of kettle
-#define hot_pump_pin A4     // Hot water
-#define motor_pin_1 2       // Sugar dispenser
-#define motor_pin_2 3       // Sugar dispenser
-#define mixer_relay_pin A1  // Mixer pin
+/* --- PINS --- */
+#define kettle_relay_pin A5  // Relay of kettle
+#define hot_pump_pin A4      // Hot water
+#define motor_pin_1 2        // Sugar dispenser
+#define motor_pin_2 3        // Sugar dispenser
+#define mixer_relay_pin A1   // Mixer pin
+#define switch_button_pin 12 // Switch button pin
 
-#define switch_button_pin 12
+/* --- Timers --- */
+int cup_pump_time = 5 * 1000;
+int sugar_spoon_time = 4 * 1000;
+int mixer_time = 5 * 1000;
 
 class button {
   public:
@@ -44,10 +49,13 @@ void setup()
 
   pinMode(kettle_relay_pin, OUTPUT);
   digitalWrite(kettle_relay_pin, LOW);
+  
   pinMode(hot_pump_pin, OUTPUT);
   digitalWrite(hot_pump_pin, LOW);
+  
   pinMode(mixer_relay_pin, OUTPUT);
   digitalWrite(mixer_relay_pin, LOW);
+  
   pinMode(motor_pin_1, OUTPUT);
   pinMode(motor_pin_2, OUTPUT);
   digitalWrite(motor_pin_1, LOW);
@@ -57,6 +65,7 @@ void setup()
 void loop()
 {
   static uint8_t stage = 0;
+  static long timer = millis();
   
   // Switch state of making tea
   if (sw_button.click()) {
@@ -64,44 +73,70 @@ void loop()
     if (stage > 4) stage = 0;
 
     Serial.println(stage);
-    switch (stage) {
+  }
+
+   switch (stage) {
+      // Kettle
       case 0:
         Serial.println("Чайник включён");
         digitalWrite(kettle_relay_pin, HIGH);
+        timer = millis();
         break;
 
+      // Pump
       case 1:
         Serial.println("Помпа включена");
         digitalWrite(kettle_relay_pin, LOW);
 
         digitalWrite(hot_pump_pin, HIGH);
+        
+        Serial.println(millis(), timer);
+        Serial.println(cup_pump_time);
+        if(millis() - timer > cup_pump_time) {
+            stage++;
+            timer = millis();
+            digitalWrite(hot_pump_pin, LOW);
+            Serial.println("Условие");
+        }
+        
         break;
 
+      // Sugar
       case 2:
         Serial.println("Дозатор сахара включён");
-        digitalWrite(hot_pump_pin, LOW);
 
         digitalWrite(motor_pin_1, LOW);
         digitalWrite(motor_pin_2, HIGH);
+
+        if(millis() - timer > sugar_spoon_time) {
+          stage++;
+          timer = millis();
+          digitalWrite(motor_pin_1, LOW);
+          digitalWrite(motor_pin_2, LOW);
+        }
+        
         break;
 
+      // Mixer
       case 3:
         Serial.println("Включаем мешалку");
 
-        digitalWrite(motor_pin_1, LOW);
-        digitalWrite(motor_pin_2, LOW);
-
         digitalWrite(mixer_relay_pin, HIGH);
+
+        if(millis() - timer > mixer_time) {
+          stage++;
+          timer = millis();
+          digitalWrite(mixer_relay_pin, LOW);
+        }
+        
         break;
 
+      // Done
       case 4:
-        digitalWrite(mixer_relay_pin, LOW);
-
         Serial.println("Чай готов!");
         break;
 
       default:
         break;
     }
-  }
 }
