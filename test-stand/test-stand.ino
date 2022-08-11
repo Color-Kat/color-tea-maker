f9129ef5a2484781165bd04bd60a3cfe7ff496a5
Color-Kat <katik7979@gmail.com>
2022-08-11 13:39:35 +0300

    get temp of water

--- test-stand/test-stand.ino
+++ test-stand/test-stand.ino
@@ -1,15 +1,24 @@
+#include <microDS18B20.h>
+
 /* --- PINS --- */
-#define kettle_relay_pin A5  // Relay of kettle
-#define hot_pump_pin A4      // Hot water
+#define kettle_relay_pin 4   // Relay of kettle
+#define termometr_pin A5     // Relay of kettle
+#define hot_pump_pin 6       // Hot water
 #define motor_pin_1 2        // Sugar dispenser
 #define motor_pin_2 3        // Sugar dispenser
 #define mixer_relay_pin A1   // Mixer pin
 #define switch_button_pin 12 // Switch button pin
 
+/* --- Temperature --- */
+#define tea_temperature 30 // Temperatur of watter for tea in the kettle
+
 /* --- Timers --- */
 int cup_pump_time = 5 * 1000;
 int sugar_spoon_time = 4 * 1000;
 int mixer_time = 5 * 1000;
+
+// Connect thermometer DS18b20
+MicroDS18B20<termometr_pin> ds; 
 
 class button {
   public:
@@ -47,15 +56,22 @@ void setup()
 {
   Serial.begin(9600);
 
+  // Kettle
   pinMode(kettle_relay_pin, OUTPUT);
   digitalWrite(kettle_relay_pin, LOW);
-  
+
+  // Thermometer
+  pinMode(termometr_pin, INPUT);
+
+  // Pump
   pinMode(hot_pump_pin, OUTPUT);
   digitalWrite(hot_pump_pin, LOW);
-  
+
+  // Mixer
   pinMode(mixer_relay_pin, OUTPUT);
   digitalWrite(mixer_relay_pin, LOW);
-  
+
+  // Sugar dispenser motor
   pinMode(motor_pin_1, OUTPUT);
   pinMode(motor_pin_2, OUTPUT);
   digitalWrite(motor_pin_1, LOW);
@@ -66,8 +82,23 @@ void loop()
 {
   static uint8_t stage = 0;
   static long timer = millis();
-  
-  // Switch state of making tea
+
+  /* --- Temperature --- */
+  static int temp = 0;
+  ds.requestTemp();
+  if(ds.readTemp())temp = ds.getTemp();
+
+  static unsigned long last_temp_request = 0;
+  if(millis() - last_temp_request > 1000) {
+      last_temp_request = millis();
+      if(ds.readTemp())
+          temp = ds.getTemp();
+    
+      ds.requestTemp();
+      Serial.println(temp);
+  }
+
+  /* --- Switch state of making tea --- */
   if (sw_button.click()) {
     stage++;
     if (stage > 4) stage = 0;
@@ -80,13 +111,17 @@ void loop()
       case 0:
         Serial.println("Чайник включён");
         digitalWrite(kettle_relay_pin, HIGH);
-        timer = millis();
+
+        if(temp >= tea_temperature){
+          stage++;
+          timer = millis();
+          digitalWrite(kettle_relay_pin, LOW);
+        }
         break;
 
       // Pump
       case 1:
         Serial.println("Помпа включена");
-        digitalWrite(kettle_relay_pin, LOW);
 
         digitalWrite(hot_pump_pin, HIGH);
         
